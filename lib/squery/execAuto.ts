431e94cd56c134d6f4a16865c343a0e0e8ec82a3
= <=>
2023-05-12 20:22:01 +0200

    1ere test001

--- lib/squery/execAuto.ts
+++ lib/squery/execAuto.ts
@@ -6,10 +6,12 @@ console.log(Config.conf);
 const relativeDir =__dirname.replace(Config.conf.rootDir,'')
 const systemDir = [path.join(relativeDir,'Start')];
 Log('log',{relativeDir , systemDir});
-[...systemDir,...Config.conf.execDir].forEach(exec)
+[...systemDir,...Config.conf.execDir.map((link)=>{
+  return path.join(...link.split('/'));
+})].forEach(exec);
 
 async function exec(directory: string) {
-  const dir = Config.conf.rootDir + directory;
+  const dir = path.join( Config.conf.rootDir , directory);
   let files = fs.readdirSync(dir);
 
   let scripts: string[] = files.filter((file) => {
